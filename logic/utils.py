16001cba6ab8f9243681790d039ae391029f1b01
Aishik ghosh <aishikghosh14@gmail.com>
2026-06-29 20:06:24 +0530

    feat: File Operations - Rename and Delete files

--- logic/utils.py
+++ logic/utils.py
@@ -43,7 +43,6 @@ def main_drives():
     for index, folder in enumerate(drives,start=1):
         print(f"{" ":<3}{f"{index}.":<5}{f"📁 {folder}"}")
         last_index = index
-        sleep(0.1)
     
     last_index+=1
     print(f"{" ":<3}{f"{last_index}.":<5}{f"❌  Exit"}")
@@ -171,6 +170,126 @@ def open_exe(file):
         print("\033[1;91mUnable to open this file\033[0m\n")
         sleep(1)      
 
+def choose_file(files):
+    clear()
+    print("\033[1;97mFILES LIST \033[0m")
+    print("\033[36m==========================================\033[0m")
+    last_index =0 
+    for index, subitem in enumerate(files, start=1):
+            icon = get_icon(subitem)
+            last_index = index
+            size = subitem.stat().st_size
+            formatted_size = format_size(size)
+            print(f"{" ":<3}{f"{index}.":<5}{f"{icon}":<3}{f"{subitem.name}":<40}{f"{formatted_size}"}")
+    last_index+=1
+    print(f"{" ":<3}{f"{last_index}.":<5}{f"❌":<3}Cancel")
+            
+    choice= 0
+    # Prompt for user's choice
+    valid = False
+    while not(valid):
+        try:
+            choice = int(input("\033[1;93mSelect a file :\033[0m\n"))
+        except ValueError:
+            print("\033[1;91mENTER A VALID INPUT\033[0m\n")
+            sleep(0.5)
+            continue
+        if (1<=choice<=last_index):
+            valid = True
+        else:
+            print(f"\033[1;91mENTER A VALID BETWEEN 1 AND {last_index}\033[0m\n")
+            sleep(0.5)
+
+    if choice == last_index:
+        return None
+        
+    return files[choice-1]
+
+def rename_file(file):
+    
+    INVALID_CHARS = '<>:"/|\\?*'
+    # Prompt for the new name
+    valid = False
+    while not(valid):
+        new_name = (input("\033[1;93mEnter the new name :\033[0m\n")).strip()
+        # check for validity of the new name
+        if (not new_name):
+            print("\033[1;91mFilename cannot be empty\033[0m\n")
+            continue
+        elif (any(char in new_name for char in INVALID_CHARS)):
+            print(f"\033[1;91m❌ Invalid filename\033[0m\n")
+            sleep(0.5)
+            continue
+
+        new_path = file.with_name(new_name + file.suffix)
+
+        if new_path.exists():
+            print("\033[1;91m❌ A file with that name already exists\033[0m\n")
+            sleep(0.5)
+            continue
+
+        try:
+            file.rename(new_path)
+            print(f"{" ":<12}{"\033[1;92m✅ FILE RENAMED SUCCESSFULLY\033[0m"}")
+            sleep(0.5)
+            print(f"\033[1;93m< OLD >\033[0m {file}  ⏩  {new_path} \033[1;93m< NEW >\033[0m")
+            sleep(1.5)
+            valid = True
+            return new_path
+        
+        except FileExistsError:
+            print(f"\033[1;91m❌ A file with that name already exists\033[0m\n")
+            sleep(0.5)
+        except PermissionError:
+            print(f"\033[1;91m❌ Permission denied\033[0m\n")
+            sleep(0.5)
+        except OSError as e:
+            print(f"\033[1;91m❌ {e}\033[0m\n")
+            sleep(0.5)
+    
+def delete_file(file):
+    clear()
+    print("\033[1;91m⚠️ Are you sure about deleting:\033[0m")
+    print(f"\033[1;97m{file}\033[0m\n")
+    sleep(0.5)
+    print("1. YES\n2. CANCEL")
+    # Prompt for the user's choice
+    valid = False
+    while not(valid):
+        try:
+            choice = int(input("\033[1;93mChoose :\033[0m\n"))
+
+        except ValueError:
+            print("\033[1;91mENTER A VALID INPUT\033[0m\n")
+            sleep(0.5)
+            continue
+        
+        if choice == 1:
+            try:
+                file.unlink() # deletes this file
+                print(f"{" ":<12}{"\033[1;92m✅ FILE DELETED SUCCESSFULLY\033[0m"}")
+                sleep(0.5)
+                return True
+
+            except PermissionError:
+                print(f"\033[1;91m❌ Permission denied\033[0m\n")
+                sleep(0.5)
+
+            except OSError as e:
+                print(f"\033[1;91m❌ {e}\033[0m\n")
+                sleep(0.5)
+            
+            return False
+        
+        elif choice == 2:
+            return False
+        
+        else:
+            print(f"\033[1;91mENTER A VALID OPTION\033[0m\n")
+            sleep(0.5)
+            
+
+
 
 def directory(drive):
     current = drive # Fetches the current directory
@@ -200,24 +319,26 @@ def directory(drive):
         print("\n")
         print("\033[1;97m FOLDERS AND FILES\033[0m")
         print("\033[36m==========================================\033[0m")
-        sleep(0.3)
         last_index = 0 
         for index, subitem in enumerate(visible_items, start=1):
             icon = get_icon(subitem)
             if subitem.is_dir():
                 print(f"{" ":<3}{f"{index}.":<5}{f"{icon}":<3}{f"{subitem.name}"}")
                 last_index = index
-                sleep(0.1)
+                
             else:
                 size = subitem.stat().st_size
                 formatted_size = format_size(size)
                 print(f"{" ":<3}{f"{index}.":<5}{f"{icon}":<3}{f"{subitem.name}":<40}{f"{formatted_size}"}")
-                sleep(0.1)
+                
                 
         if len(files) > 1:
             visible_items.append("🗃️   Organize Files")
-            visible_items.append("🔁   Rename File")
+            visible_items.append("🔁  Rename File")
             visible_items.append("🗑️   Delete File")
+        if len(folders) > 1:
+            visible_items.append("🔁  Rename Folder")
+            visible_items.append("🗑️   Delete Folder")
 
         if current != drive:
             visible_items.append("⬅️   Go Back")
@@ -225,7 +346,6 @@ def directory(drive):
         visible_items.append("⬅️   Back To Drive Menu")
         visible_items.append("❌  Exit")
         
-        sleep(0.2)
         print("\n")
         print("\033[1;97m ACTIONS\033[0m")
         print("\033[36m==========================================\033[0m")
@@ -233,7 +353,7 @@ def directory(drive):
             if not isinstance(subitem,(Path)) or not subitem.is_file():
                 last_index = index
                 print(f"{" ":<3}{f"{index}.":<5}{f"{subitem}"}")
-                sleep(0.1)
+                # sleep(0.1)
         
         choice=0
         # Prompt for user's choice
@@ -305,9 +425,83 @@ def directory(drive):
             sleep(0.5)
             return "Drive Menu"
 
-        elif "Exit" in str(selected): # If a folder is selected 
+        elif "Exit" in str(selected): 
             exit_app()
 
-        
+        elif "Rename File" in str(selected):
+            file = choose_file(files) # For user to choose a file
+            if file is None:
+                continue # redraws the directory menu 
+
+            while True:
+                clear()
+                print("\033[1;93mSelected File:\033[0m")
+                print(f"\033[1;97m{file}\033[0m\n")
+                sleep(0.5)
+                print("1.✅  Rename this file")
+                print("2.⬅️   Choose another file")
+                print("3.❌ Cancel")
+
+                try:
+                    choice = int(input("\033[1;93mChoose :\033[0m\n"))
+                except ValueError:
+                    print("\033[1;91mENTER A VALID INPUT\033[0m\n")
+                    sleep(0.5)
+                    continue
+
+                if choice == 1:
+                    file = rename_file(file)
+                    sleep(0.5)
+                    break # Leave the rename menu
+
+                elif choice == 2:
+                    file = choose_file(files) # select another file
+
+                elif choice == 3:
+                   break
+
+                else:
+                    print("\033[1;91mENTER A VALID OPTION\033[0m\n")
+                    sleep(0.5)
+                    continue
+                    
+        elif "Delete File" in str(selected):
+            file = choose_file(files) # For user to choose a file
+            if file is None:
+                continue # redraws the directory menu 
+
+            while True:
+                clear()
+                print("\033[1;93mSelected File:\033[0m")
+                print(f"\033[1;97m{file}\033[0m\n")
+                sleep(0.5)
+                print("1.✅  Delete this file")
+                print("2.⬅️   Choose another file")
+                print("3.❌ Cancel")
+
+                try:
+                    choice = int(input("\033[1;93mChoose :\033[0m\n"))
+                except ValueError:
+                    print("\033[1;91mENTER A VALID INPUT\033[0m\n")
+                    sleep(0.5)
+                    continue
+
+                if choice == 1:
+                    if(delete_file(file)):
+                        sleep(0.5)
+                        break # Leave the rename menu
+                    else:
+                        continue # Stay in the delete menu
+
+                elif choice == 2:
+                    file = choose_file(files) # select another file
+
+                elif choice == 3:
+                   break
+
+                else:
+                    print("\033[1;91mENTER A VALID OPTION\033[0m\n")
+                    sleep(0.5)
+                    continue
         
         
